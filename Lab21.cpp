8a74b95c03495eb2d0d9b6f1c9c36d0be7a940a5
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-13 12:08:31 -0700

    Commit 2

--- Lab21.cpp
+++ Lab21.cpp
@@ -19,4 +19,16 @@ class Goat {
         string colors[15] = {"Red", "Blue", "Green", "Yellow", "Brown", "Black", "Purple", "Pink", "Magenta", "Cyan", "Teal", "Gray", "White", "Orange", "Teal"}; 
 
       public: 
+
+         Goat() {
+
+            age = rand() % 20 + 1; 
+
+            color = colors[rand() % 15];
+
+            name = names[rand() % 15]; 
+
+        }
+
+        Goat(int age, string color, string name) : age(age), name(name), color(color) {}
 }; 
