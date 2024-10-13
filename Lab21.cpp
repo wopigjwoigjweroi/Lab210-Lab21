e3d59eb2f933a1b87208a7fe955f9e965a70af7e
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-13 12:02:08 -0700

    Commit 1

---
+++ Lab21.cpp
@@ -0,0 +1,22 @@
+// COMSC-210 | Lab-21 | Jeremiah Ortiz
+#include <iostream>
+#include <cstdlib> 
+using namespace std; 
+
+
+class Goat {
+
+    private: 
+
+        int age; 
+
+        string name; 
+
+        string color; 
+
+        string names[15] = {"Mark", "Steve", "Jack", "Hamil", "Jake", "Paul", "Abram", "Jacob", "Michael", "Frank", "Henry", "David", "Alice", "Bob", "Mia"}; 
+
+        string colors[15] = {"Red", "Blue", "Green", "Yellow", "Brown", "Black", "Purple", "Pink", "Magenta", "Cyan", "Teal", "Gray", "White", "Orange", "Teal"}; 
+
+      public: 
+}; 
