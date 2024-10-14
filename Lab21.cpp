b22254f1bac8d32201f459ee9368d57f199c051f
wopigjwoigjweroi <agizekel@gmail.com>
2024-10-13 17:35:43 -0700

    Commit 4

--- Lab21.cpp
+++ Lab21.cpp
@@ -48,8 +48,6 @@ class Goat {
         }
 }; 
 
-
-
     class DoublyLinkedList {
 
         private: 
@@ -62,7 +60,7 @@ class Goat {
 
         Node* next; 
 
-        Node(Goat val, Node* p = nullptr, Node* n = nullptr)  {}
+        Node(Goat val, Node* p = nullptr, Node* n = nullptr) : data(d), prev(p), next(n) {}
 
 };
             Node* head; 
@@ -70,5 +68,83 @@ class Goat {
             Node* tail; 
 
         public:
+
+ DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+ void push_back(Goat value) {
+
+     Node* newNode = new Node(value);
+
+     if (!tail) {
+
+         tail = head = newNode;
+     }
+     else {
+
+         tail->next = newNode;
+
+         newNode->prev = tail;
+
+         tail = newNode;
+     }
+ }
+
+ void push_front(Goat value) {
+
+     Node* newNode = new Node(value);
+
+     if (!head) {
+
+         head = tail = newNode;
+     }
+     else {
+
+         newNode->next = head;
+
+         head->prev = newNode;
+
+         head = newNode;
+     }
+ }
+
+ void print_forward() {
+
+     Node* current = head;
+
+     while (current) {
+
+         cout << current->data.getName() << " (" << current->data.getColor() << "," << current->data.getAge() << ")" << endl;
+
+         current = current->next;
+     }
+
+     cout << endl;
+ }
+
+
+ void print_reverse() {
+
+     Node* current = tail;
+
+     while (current) {
+
+         cout << current->data.getName() << " ( " << current->data.getColor() << " ," << current->data.getAge() << " )" << endl;
+
+         current = current->prev;
+     }
+
+     cout << endl;
+ }
+
+ ~DoublyLinkedList() {
+     while (head) {
+
+         Node* temp = head;
+
+         head = head->next;
+
+         delete temp;
+     }
+ }
 
 }; 
